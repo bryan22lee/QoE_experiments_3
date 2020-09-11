7df27a5a1bbbdf67b0653c4ab14d1aaff97e5f84
Bryan Lee <bryan22lee@gmail.com>
2020-09-11 00:35:31 -0500

    update

--- controllers/start.js
+++ controllers/start.js
@@ -35,7 +35,7 @@ var post_start = async (ctx, next) => {
     var age = ctx.request.body.age;
     var network = ctx.request.body.network;
     // Show example videos first: 1 (good quality example), 2 (bad quality example), the rest are test videos
-    var video_order = [1, 2, ...getOder(3, num_vids)];
+    var video_order = [1, 2, ...getOder(3,num_vids)];
     console.log(mturkID, device, age);
     var start = new Date().getTime();
    
